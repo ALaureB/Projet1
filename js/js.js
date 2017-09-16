97e3571fc66708662bc2dd623bc3177bb14a289f
Anne-Laure Bienaimé <annelaure.bienaime@gmail.com>
2017-09-16 18:35:25 +0200

    Home page structuration + carrousel bugfix

--- js/js.js
+++ js/js.js
@@ -1,3 +1,18 @@
+// Fil Instragram
+var userFeed = new Instafeed({
+	get: 'user',
+	userId: '1188398327',
+	clientId: '1bd87e6cc22e47cfad37d35273e35634',
+	accessToken: '1188398327.1677ed0.ca6dd113fa6143f4a6ff0aec79dfefab',
+	resolution: 'standard_resolution',
+	template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
+	sortBy: 'most-recent',
+	limit: 6,
+	links: false
+});
+userFeed.run();
+
+// Grille photo
 var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
   optionspercentPosition: true,
